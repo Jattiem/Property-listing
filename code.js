4efd6a00a3181a13f2f5b0477d06c8275d0aef2f
jattiemyaseen <yaseenjattiem0000@gmail.com>
2022-07-01 11:24:53 +0200

    d

--- code.js
+++ code.js
@@ -1,174 +1,175 @@
+
 let propertyFilter = document.getElementById("FilterByProperty");
 let locationFilter = document.getElementById("FilterByLocation");
 let propertySizeFilter = document.getElementById("FilterBySize");
 let budgetFilter = document.getElementById("FilterByBudget");
 prop = localStorageProp();
 
-function localStorageProp() {
+function localStorageProp(){
     return JSON.parse(localStorage.getItem('property'));
 }
 
-localStorage.setItem('property',JSON.stringify(properties = [{
-    id:1,
-    type:"Studio",
-    category:'Properties',
-    title:'Renovated Studio',
-    location:'New York',
-    Area:'194 Mercer Street, 627 Broadway, New York, NY',
-    Bedrooms:4,
-    Bathrooms:2,
-    Garage:1,
-    yearBuilt:'2016',
-    SqFt:'1200',
-    price:540000,
-    pricePer:3700,
-    url:'./images/Studio.jpg'
-    
-},
-{
-    id:2,
-    type:"Apartment",
-    category:'Properties',
-    title:'New Apartment Nice View',
-    location:'Chicago',
-    Area:'8100 S Ashland Ave, Chicago, IL 60620, USA',
-    Bedrooms:3,
-    Bathrooms:1,
-    Garage:1,
-    yearBuilt:'2016',
-    SqFt:'2',
-    price:'23444',
-    pricePer:11000,
-    url:'./images/Apartment.jpg'
-},
-{
-    id:3,
-    type:'Comercial',
-    category:'Properties',
-    title:'comercial building',
-    location:'New York',
-    Area:'Quincy St, Brooklyn, NY, USA',
-    Bedrooms:4,
-    Bathrooms:6,
-    Garage:7,
-    yearBuilt:'2011',
-    SqFt:'25560',
-    price:8766000,
-    pricePer:7600,
-    url:'./images/Comercial.jpg'
-},
-{
-    id:4,
-    type:'Office',
-    category:'Properties',
-    title:'Office In Downtown',
-    location:'Los Angeles',
-    Area:'1417 Glendale Blvd, Los Angeles, CA 90026',
-    Bedrooms:'2',
-    Bathrooms:'4',
-    Garage:'6',
-    yearBuilt:'2017',
-    SqFt:'3100',
-    price:'20000',
-    pricePer:7600,
-    url:'./images/Office.jpg'
-},
-{
-    id:5,
-    type:'Single Family Home',
-    category:'Properties',
-    title:'Guaranteed Modern Home',
-    location:'Miami',
-    Area:'905 Brickell Bay Dr, Miami, FL 33131, USA',
-    Bedrooms:3,
-    Bathrooms:2,
-    Garage:1,
-    yearBuilt:'2016',
-    SqFt:'3410',
-    price:590000,
-    pricePer:3500,
-    url:'./images/Single-Family-Home.jpg'
-},
-{
-    id:6,
-    type:'Villa',
-    category:'Properties',
-    title:'Villa With Pool',
-    location:'Chicago',
-    Area:'6111 Brynhurst Ave, Los Angeles, CA 90043',
-    Bedrooms:4,
-    Bathrooms:1,
-    Garage:1,
-    yearBuilt:'2016',
-    SqFt:'3410',
-    price:990000,
-    pricePer:5400,
-    url:'./images/Villa.jpg'
-},
-{
-    id:7,
-    type:'Shop',
-    category:'Properties',
-    title:'Small Shop',
-    location:'New York',
-    Area:'Vesey St, New York, NY, USA',
-    Bedrooms:'5',
-    Bathrooms:'3',
-    Garage:'5',
-    yearBuilt:'2016',
-    SqFt:'1200',
-    price:890000,
-    pricePer:3690,
-    url:'./images/Shop.jpg'
-},
-{
-    id:8,
-    type:'Multi Family Home',
-    category:'Properties',
-    title:'Big multi home',
-    location:'New York',
-    Area:'Vesey St, New York, NY, USA',
-    Bedrooms:6,
-    Bathrooms:4,
-    Garage:4,
-    yearBuilt:'2016',
-    SqFt:'1200',
-    price:9000000,
-    pricePer:10000,
-    url:'./images/Multi-Family-Home.jpg'
-},
-{
-    id:9,
-    type:'Condo',
-    category:'Properties',
-    title:'Condo on top',
-    location:'Miami',
-    Area:'101 Densel Bay Dr, Miami, FL 123431, USA',
-    Bedrooms:3,
-    Bathrooms:2,
-    Garage:2,
-    yearBuilt:'2015',
-    SqFt:'1200',
-    price:760000,
-    pricePer:7860,
-    url:'./images/Condo.jpg'
-},
-{
-    id:10,
-    type:'Residential',
-    category:'Properties',
-    title:'Standard',
-    location:'Chicago',
-    Area:'2300 S Bashland Ave, Chicago, IL 45620, USA',
-    Bedrooms:3,
-    Bathrooms:2,
-    Garage:1,
-    yearBuilt:'2018',
-    SqFt:'1300',
-    price:762330,
-    pricePer:7360,
-    url:'./images/Residential.jpg'
-}
+localStorage.setItem('property',JSON.stringify(properties = 
+    [{
+        id:1,
+        type:"Studio",
+        category:'Properties',
+        title:'Renovated Studio',
+        location:'New York',
+        Area:'194 Mercer Street, 627 Broadway, New York, NY',
+        Bedrooms:4,
+        Bathrooms:2,
+        Garage:1,
+        yearBuilt:'2016',
+        SqFt:'1200',
+        price:540000,
+        pricePer:3700,
+        url:'./images/Studio.jpg'       
+    },
+    {
+        id:2,
+        type:"Apartment",
+        category:'Properties',
+        title:'New Apartment Nice View',
+        location:'Chicago',
+        Area:'8100 S Ashland Ave, Chicago, IL 60620, USA',
+        Bedrooms:3,
+        Bathrooms:1,
+        Garage:1,
+        yearBuilt:'2016',
+        SqFt:'2',
+        price:'23444',
+        pricePer:11000,
+        url:'./images/Apartment.jpg'
+    },
+    {
+        id:3,
+        type:'Comercial',
+        category:'Properties',
+        title:'comercial building',
+        location:'New York',
+        Area:'Quincy St, Brooklyn, NY, USA',
+        Bedrooms:4,
+        Bathrooms:6,
+        Garage:7,
+        yearBuilt:'2011',
+        SqFt:'25560',
+        price:8766000,
+        pricePer:7600,
+        url:'./images/Comercial.jpg'
+    },
+    {
+        id:4,
+        type:'Office',
+        category:'Properties',
+        title:'Office In Downtown',
+        location:'Los Angeles',
+        Area:'1417 Glendale Blvd, Los Angeles, CA 90026',
+        Bedrooms:'2',
+        Bathrooms:'4',
+        Garage:'6',
+        yearBuilt:'2017',
+        SqFt:'3100',
+        price:'20000',
+        pricePer:7600,
+        url:'./images/Office.jpg'
+    },
+    {
+        id:5,
+        type:'Single Family Home',
+        category:'Properties',
+        title:'Guaranteed Modern Home',
+        location:'Miami',
+        Area:'905 Brickell Bay Dr, Miami, FL 33131, USA',
+        Bedrooms:3,
+        Bathrooms:2,
+        Garage:1,
+        yearBuilt:'2016',
+        SqFt:'3410',
+        price:590000,
+        pricePer:3500,
+        url:'./images/Single-Family-Home.jpg'
+    },
+    {
+        id:6,
+        type:'Villa',
+        category:'Properties',
+        title:'Villa With Pool',
+        location:'Chicago',
+        Area:'6111 Brynhurst Ave, Los Angeles, CA 90043',
+        Bedrooms:4,
+        Bathrooms:1,
+        Garage:1,
+        yearBuilt:'2016',
+        SqFt:'3410',
+        price:990000,
+        pricePer:5400,
+        url:'./images/Villa.jpg'
+    },
+    {
+        id:7,
+        type:'Shop',
+        category:'Properties',
+        title:'Small Shop',
+        location:'New York',
+        Area:'Vesey St, New York, NY, USA',
+        Bedrooms:'5',
+        Bathrooms:'3',
+        Garage:'5',
+        yearBuilt:'2016',
+        SqFt:'1200',
+        price:890000,
+        pricePer:3690,
+        url:'./images/Shop.jpg'
+    },
+    {
+        id:8,
+        type:'Multi Family Home',
+        category:'Properties',
+        title:'Big multi home',
+        location:'New York',
+        Area:'Vesey St, New York, NY, USA',
+        Bedrooms:6,
+        Bathrooms:4,
+        Garage:4,
+        yearBuilt:'2016',
+        SqFt:'1200',
+        price:9000000,
+        pricePer:10000,
+        url:'./images/Multi-Family-Home.jpg'
+    },
+    {
+        id:9,
+        type:'Condo',
+        category:'Properties',
+        title:'Condo on top',
+        location:'Miami',
+        Area:'101 Densel Bay Dr, Miami, FL 123431, USA',
+        Bedrooms:3,
+        Bathrooms:2,
+        Garage:2,
+        yearBuilt:'2015',
+        SqFt:'1200',
+        price:760000,
+        pricePer:7860,
+        url:'./images/Condo.jpg'
+    },
+    {
+        id:10,
+        type:'Residential',
+        category:'Properties',
+        title:'Standard',
+        location:'Chicago',
+        Area:'2300 S Bashland Ave, Chicago, IL 45620, USA',
+        Bedrooms:3,
+        Bathrooms:2,
+        Garage:1,
+        yearBuilt:'2018',
+        SqFt:'1300',
+        price:762330,
+        pricePer:7360,
+        url:'./images/Residential.jpg'
+    },
 ]));
 
 function getProperties(prop){
@@ -201,6 +202,7 @@ function getProperties(prop){
 }
 getProperties(properties);
 
+
 function filterAll(){
     let currentItems = localStorageProp();
     
@@ -211,32 +213,31 @@ function filterAll(){
     }else{
         filteredProperties = currentItems.filter( x=>{
             return x.type == propertyFilter.value;
-        })
+        });
     }
     
+    if(locationFilter.value == "Any"){
+        filteredLocations = currentItems;
+    }else{
+        filteredLocations = currentItems.filter( x=>{
+            return x.Area.indexof(locationFilter.value);
+        });
+    }
     
-    // if(locationFilter.value == "AllCities"){
-    //     filteredLocations = currentItems;
-    // }else{
-    //     filteredLocations = currentItems.filter( x=>{
-    //         return x.Area.indexof(locationFilter.value);
-    //     })
-    // }
-    
-    // if(propertySizeFilter.value == "Any"){
-    //     filteredSizes = currentItems;
-    // }else{
-    //     filteredSizes = currentItems.filter( x=>{
-    //         return x.Bedrooms <= propertySizeFilter.value;
-    //     })
-    // }
-    // if(budgetFilter.value == "Any"){
-    //     filteredBudget = currentItems;
-    // }else{
-    //     filteredBudget = currentItems.filter( x=>{
-    //         return x.price <= budgetFilter.value;
-    //     })
-    // }
+    if(propertySizeFilter.value == "Any"){
+        filteredSizes = currentItems;
+    }else{
+        filteredSizes = currentItems.filter( x=>{
+            return x.Bedrooms <= propertySizeFilter.value;
+        });
+    }
+    if(budgetFilter.value == "Any"){
+        filteredBudget = currentItems;
+    }else{
+        filteredBudget = currentItems.filter( x=>{
+            return x.price <= budgetFilter.value;
+        });
+    }
 
 
     const allFilters = [filteredProperties, filteredLocations ,filteredSizes ,filteredBudget];
@@ -270,6 +271,3 @@ function filterAll(){
         getProperties(filter);
     }
 };
-
-
-
